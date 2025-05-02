01828ba0fc7dc48154728b398d414afde64024b2
rastudillon <137467194+rastudillon@users.noreply.github.com>
2025-05-02 19:44:11 -0400

    Update dashboard3.py

--- dashboard3.py
+++ dashboard3.py
@@ -552,8 +552,8 @@ def dashboard_anual(df):
 
     # 4) Mapea al nombre en español
     dicc_meses = {
-    1:"Enero",2:"Febrero",3:"Marzo",4:"Abril",5:"Mayo",6:"Junio",
-    7:"Julio",8:"Agosto",9:"Septiembre",10:"Octubre",11:"Noviembre",12:"Diciembre"
+        1:"Enero",2:"Febrero",3:"Marzo",4:"Abril",5:"Mayo",6:"Junio",
+        7:"Julio",8:"Agosto",9:"Septiembre",10:"Octubre",11:"Noviembre",12:"Diciembre"
     }
     último_mes_nombre = dicc_meses[mes_num]
     
